3b6ae6d294a60b07926f4be76a490f8496554ee0
sairishisambaraju <rishi85sai@gmail.com>
2026-07-30 17:45:08 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(page_title="Netflix Data Visualization", layout="wide")
+
+st.title("🎬 Netflix Data Visualization Project")
+st.write("Created by Sai Rishi Sambaraju")
+
+st.markdown("""
+## Project Overview
+
+This project analyzes the Netflix Titles dataset using Python.
+
+### Tools Used
+- Python
+- Pandas
+- Matplotlib
+- Plotly
+- Streamlit
+
+### Visualizations
+- Movies vs TV Shows
+- Top Countries
+- Ratings Distribution
+- Release Year Analysis
+- Genres
+- Directors
+- Duration Analysis
+""")
